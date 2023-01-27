09de6bb30f58b8ccd63f715d30309c14b0672462
Florencia D'Onofrio <flordonofrio2002@gmail.com>
2023-01-26 19:43:36 -0800

    style-randomcodes

--- js/randomcodes.js
+++ js/randomcodes.js
@@ -7,13 +7,13 @@ function generateCode (){
     //Generate character multiple times using a loop
     for (i=1; i<=8; i++){
         var char = Math.random() * str.length; //Random selectoin from str
-        code += str.charAt (char) //Acumulate the generated character in a str of eight
+        code += str.charAt (char); //Acumulate the generated character in a str of eight
     }
-    return code //Resturn the accumulated str when loop ends
+    return code; //Resturn the accumulated str when loop ends
 }
 
 //Get HTML element to display
-document.getElementById("codes").innerHTML = generateCode ();
+document.getElementById("codes").innerHTML = generateCode();
 
 //Disable Buttom
 function disableButtom (){
